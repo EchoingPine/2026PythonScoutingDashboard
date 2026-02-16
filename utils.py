f990d092d91d5d0b2e15ca6400ad68cb0b59acea
EchoingPine <smchimu@gmail.com>
2026-02-16 01:01:24 -0500

    a

--- utils.py
+++ utils.py
@@ -116,7 +116,7 @@ def plot_team_scores(team_number, show_table=False, dataType=""):
         team_data.drop(columns='Scouter Initials', inplace=True, errors='ignore')
         team_data.drop(columns='Team Number', inplace=True, errors='ignore')
 
-        pit_data.drop(columns='Names', inplace=True, errors='ignore')
+        pit_data.drop(columns='Name(s)', inplace=True, errors='ignore')
         pit_data.drop(columns='Team #', inplace=True, errors='ignore')
 
         # Transpose pit data for display
